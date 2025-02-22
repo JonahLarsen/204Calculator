af05a477e454b94a70c02fff0ad272da9b173c09
Jonah Larsen <Jonah.Larsen@gmail.com>
2025-02-21 19:49:05 -0700

    Added main code to run commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Testing dockerfile");
+    Scanner scanner = new Scanner(System.in);
+    Calculator calculator = new Calculator();
+    System.out.println("Please enter your command and parameters: ");
+    String input = "";
+    while (!input.equals("exit")) {
+      input = scanner.nextLine();
+      String[] split = input.split(" ");
+      switch(split[0]) {
+        case "add":
+          System.out.println(calculator.add(Integer.parseInt(split[1]), Integer.parseInt(split[2])));
+          break;
+        case "subtract":
+          System.out.println(calculator.subtract(Integer.parseInt(split[1]), Integer.parseInt(split[2])));
+          break;
+        case "multiply":
+          System.out.println(calculator.multiply(Integer.parseInt(split[1]), Integer.parseInt(split[2])));
+          break;
+        case "divide":
+          System.out.println(calculator.divide(Integer.parseInt(split[1]), Integer.parseInt(split[2])));
+          break;
+        case "fibonacci":
+          System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(split[1])));
+          break;
+        case "binary":
+          System.out.println(calculator.intToBinaryNumber(Integer.parseInt(split[1])));
+          break;
+      }
+    }
+    scanner.close();
   }
 }
